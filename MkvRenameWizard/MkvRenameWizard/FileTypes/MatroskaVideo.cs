ebbc0e91712bb37eeefafb9bd7fd4e2241471031
Nick Cicchetti <nc_cicchetti@yahoo.com>
2026-05-20 16:00:55 -0400

    added mkv file container type

--- MkvRenameWizard/MkvRenameWizard/FileTypes/MatroskaVideo.cs
+++ MkvRenameWizard/MkvRenameWizard/FileTypes/MatroskaVideo.cs
@@ -1,6 +1,35 @@
+using System.IO;
+using FileTypeChecker;
+using FileTypeChecker.Abstracts;
+
 namespace MkvRenameWizard.FileTypes;
 
-public class MatroskaVideo
+public class MatroskaVideo : FileType
 {
+    private static readonly byte[] MkvMagicNumber = [0x1A, 0x45, 0xDF, 0xA3];
     
+    public MatroskaVideo() 
+        : base("Matroska Video File", "video/x-matroska", "mkv", MkvMagicNumber)
+    {
+    }
+    
+    public MatroskaVideo(string name, string mimeType, string extension, byte[] magicBytes) 
+        : base(name, mimeType, extension, magicBytes)
+    {
+    }
+
+    public MatroskaVideo(string name, string mimeType, string extension, byte[][] magicBytes) 
+        : base(name, mimeType, extension, magicBytes)
+    {
+    }
+
+    public MatroskaVideo(string name, string mimeType, string extension, MagicSequence magicBytes) 
+        : base(name, mimeType, extension, magicBytes)
+    {
+    }
+
+    public MatroskaVideo(string name, string mimeType, string extension, MagicSequence[] magicBytesSequence) 
+        : base(name, mimeType, extension, magicBytesSequence)
+    {
+    }
 }
